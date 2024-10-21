0a160b8d8483adcec8e1485cf1082570abe86c9b
Gottschali <aligottschall@protonmail.com>
2024-10-21 16:16:28 +0200

    readonly/editable code blocks

--- editor.js
+++ editor.js
@@ -5,22 +5,16 @@ window.editors = [];
         return;
     }
 
-    Array.from(document.querySelectorAll('.editable')).forEach(function(editable) {
-        let display_line_numbers = window.playground_line_numbers || false;
+    let code_nodes = Array
+        .from(document.querySelectorAll('code'))
+        .filter(n => n.parentElement.tagName == "PRE")
 
-        let editor = ace.edit(editable);
-        editor.setOptions({
-            highlightActiveLine: false,
-            showPrintMargin: false,
-            showLineNumbers: display_line_numbers,
-            showGutter: display_line_numbers,
-            maxLines: Infinity,
-            fontSize: "0.875em" // please adjust the font size of the code in general.css
-        });
+    code_nodes.forEach(function(code_block) {
 
         // Extract the language of the code block from the class name
         let language = "gobra"
-        for (const cls of new Array(...editable.classList)) {
+        let editable = code_block.classList.contains("editable")
+        for (const cls of new Array(...code_block.classList)) {
             if (cls.startsWith("language-"))  {
                 language = cls.replace("language-", "")
             }
@@ -29,6 +23,31 @@ window.editors = [];
             language = "golang"
         }
 
+        let hide = code_block.classList.contains("hide-boring")
+        if (hide) {
+            code_block.classList.add('hljs')
+            // code_block.classList.remove('language-gobra')
+            // code_block.classList.remove('language-go')
+            // hljs.highlightBlock(code_block)
+            return
+        }
+
+        let editor = ace.edit(code_block);
+        let display_line_numbers = window.playground_line_numbers || false;
+
+        editor.setOptions({
+            readOnly: editable,
+            highlightActiveLine: editable,
+            highlightGutterLine: editable,
+            showPrintMargin: false,
+            showLineNumbers: display_line_numbers,
+            showGutter: display_line_numbers,
+            maxLines: Infinity,
+            fontSize: "0.875em" // please adjust the font size of the code in general.css
+        });
+        if (editable) {
+            editor.renderer.$cursorLayer.element.style.opacity=0
+        }
 
         editor.$blockScrolling = Infinity;
 
